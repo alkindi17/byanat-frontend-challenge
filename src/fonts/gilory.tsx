73acad480880c9830619cb112baaa5d7587c93cd
Ali Al Kindi <alkindia1@cardiff.ac.uk>
2024-06-14 10:21:39 +0400

    Add Gilory font

---
+++ src/fonts/gilory.tsx
@@ -0,0 +1,106 @@
+import localFont from "next/font/local";
+
+export const gilory = localFont({
+  src: [
+    {
+      path: "../../public/fonts/gilory/Gilroy-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-ThinItalic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-UltraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-UltraLightItalic.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-LightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-RegularItalic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-ExtraBoldItalic.ttf",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-BlackItalic.ttf",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-Heavy.ttf",
+      weight: "950",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gilory/Gilroy-HeavyItalic.ttf",
+      weight: "950",
+      style: "italic",
+    },
+  ],
+});
