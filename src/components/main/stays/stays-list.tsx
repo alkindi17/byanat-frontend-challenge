d24b3be6e7ff26a55cf2c8307be330398ac550ff
Ali Al Kindi <alkindia1@cardiff.ac.uk>
2024-06-15 16:22:23 +0400

    Update stays list component props name for clarity

--- src/components/main/stays/stays-list.tsx
+++ src/components/main/stays/stays-list.tsx
@@ -8,13 +8,17 @@ export type StaysListType = Stay[];
 
 /**
  * Renders a list of stays.
- * @param stayList - The list of stays to render.
+ * @param staysListData - The list of stays to render.
  * @returns The rendered Stays list component.
  */
-export default function StaysList({ stayList }: { stayList: StaysListType }) {
+export default function StaysList({
+  staysListData,
+}: {
+  staysListData: StaysListType;
+}) {
   return (
     <div className="grid grid-flow-row auto-rows-fr gap-6 pb-10">
-      {stayList.map((stay) => (
+      {staysListData.map((stay) => (
         <StayCard
           key={stay.id}
           id={stay.id}
--- src/components/main/stays/stays.tsx
+++ src/components/main/stays/stays.tsx
@@ -2,9 +2,7 @@ import StaysList, { StaysListType } from "./stays-list";
 import staysJson from "../../../../public/data/stays.json";
 
 // Convert the JSON data to types of list of stays.
-const stays: StaysListType = staysJson.map((stay: any) => ({
-  ...stay,
-}));
+const staysListData = staysJson as StaysListType;
 
 /**
  * Renders the Stays component, displaying a list of stays.
@@ -14,7 +12,7 @@ export default function Stays() {
   return (
     <div className="flex h-full flex-col gap-8 overflow-scroll pe-6 pt-10">
       <h1 className="text-4xl font-extrabold">Stays in Los Angeles</h1>
-      <StaysList stayList={stays} />
+      <StaysList staysListData={staysListData} />
     </div>
   );
 }
