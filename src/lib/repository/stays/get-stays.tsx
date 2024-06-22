d387f288c13d8788b1def324c817ca02a47099f1
Ali Al Kindi <alkindia1@cardiff.ac.uk>
2024-06-22 12:48:56 +0400

    Update file path for stays.json in getStays function

--- src/lib/repository/stays/get-stays.tsx
+++ src/lib/repository/stays/get-stays.tsx
@@ -12,7 +12,10 @@ import { Stays } from "@/components/main/stays/stays-list";
 export default async function getStays() {
   // Read the current stays from the json file.
   const stays: Stays = JSON.parse(
-    fs.readFileSync(path.join("../../../../public/data/stays.json"), "utf-8"),
+    fs.readFileSync(
+      path.join(process.cwd(), "public", "data", "stays.json"),
+      "utf-8",
+    ),
   );
 
   return stays;
